9c44096bb4d18fafd46eaaebe0159c30c9fcdea7
ShunyaYokoe <nkcgta.shunya.yokoe@gmail.com>
2019-09-20 10:21:01 +0900

    enemyScript追加

---
+++ Onryo/Assets/script/Onryocon.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Onryocon : MonoBehaviour {
+
+    public float speed = 1f;
+    public float rotationspeed = 1f;
+    public float posrange = 10.0f;
+    private Vector3 targetpos;
+    private float changetarget = 50f;
+    public float targetdistance;
+
+    public Transform target; //追いかける対象
+    public Rigidbody rb;
+
+    private Vector3 vec;
+
+    Vector3 GetRandomPosition(Vector3 currentpos)
+    {
+        return new Vector3(Random.Range(-posrange + currentpos.x, posrange + currentpos.x),
+            0, Random.Range(-posrange + currentpos.z, posrange + currentpos.z));
+    }
+
+    void haikai()
+    {
+        if (targetdistance < changetarget) targetpos =
+                    GetRandomPosition(transform.position);
+
+        Quaternion targetRotation =
+            Quaternion.LookRotation(targetpos - transform.position);
+        transform.rotation =
+            Quaternion.Slerp(transform.rotation, targetRotation,
+            Time.deltaTime * rotationspeed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        Debug.Log("haikai");
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        targetpos = GetRandomPosition(transform.position);
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        targetdistance = Vector3.SqrMagnitude(transform.position - targetpos);
+        haikai();
+
+        //座標取得
+        Vector3 tmp = GameObject.Find("Onryo").transform.position;
+        GameObject.Find("Onryo").transform.position = new Vector3(tmp.x, tmp.y, tmp.z);
+        float x = tmp.x;
+        float y = tmp.y;
+        float z = tmp.z;
+
+        //プレイヤ座標取得
+        Vector3 tmp2 = GameObject.Find("Player").transform.position;
+        GameObject.Find("Player").transform.position = new Vector3(tmp2.x, tmp2.y, tmp2.z);
+        float x2 = tmp2.x;
+        float y2 = tmp2.y;
+        float z2 = tmp2.z;
+
+        if (tmp2.x - tmp.x <= 5 || tmp2.x - tmp.x >= -5 & tmp2.z - tmp.z >= 5)
+        {
+            //targetの向きに少しずつ向きが変わる
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position), 0.3f);
+
+            speed = 0.2f;
+
+            //targetに向かって進む
+            transform.position += transform.forward * speed;
+            //rb.AddForce(a * speed, 0, b * speed);
+
+        }
+        else
+        {
+            speed = 1.0f;
+            targetdistance = Vector3.SqrMagnitude(transform.position - targetpos);
+            haikai();
+        }
+       
+
+
+    }
+}
---
+++ Onryo/Assets/script/enemmove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemmove : MonoBehaviour {
+
+    public float speed = 1f;
+    public float rotationspeed = 1f;
+    public float posrange = 10.0f;
+    private Vector3 targetpos;
+    private float changetarget = 50f;
+    public float targetdistance;
+
+    Vector3 GetRandomPosition(Vector3 currentpos)
+    {
+        //return new Vector3(Random.Range(-posrange + currentpos.x, posrange + currentpos.x),
+        //    0, Random.Range(-posrange + currentpos.z, posrange + currentpos.z));
+
+        return new Vector3(10,0,10);
+    }
+
+    void haikai()
+    {
+        if (targetdistance < changetarget) targetpos =
+                    GetRandomPosition(transform.position);
+
+        Quaternion targetRotation =
+            Quaternion.LookRotation(targetpos - transform.position);
+        transform.rotation =
+            Quaternion.Slerp(transform.rotation, targetRotation,
+            Time.deltaTime * rotationspeed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        //Debug.Log("haikai");
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        targetpos = GetRandomPosition(transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        targetdistance = Vector3.SqrMagnitude(transform.position - targetpos);
+        haikai();
+	}
+}
