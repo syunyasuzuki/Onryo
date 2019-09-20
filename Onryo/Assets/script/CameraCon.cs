3989e9f4df5f44fb3cbbf03176abee09a85fdd0d
ogihararyuki <nkcgta.ogihara.ryuki@gmail.com>
2019-09-20 09:51:33 +0900

    Player及びscriptの追加 Player完成（仮）

---
+++ Onryo/Assets/script/CameraCon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCon : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject Camera;
+    private Transform PlayerTransform;
+    private Transform CameraTransform;
+    float X_Rotation;
+    float Y_Rotation;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        PlayerTransform = transform.parent;
+        CameraTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        X_Rotation = Input.GetAxis("Mouse X");
+        Y_Rotation = Input.GetAxis("Mouse Y");
+        PlayerTransform.transform.Rotate(0, X_Rotation, 0);
+        //CameraTransform.transform.Rotate(-Y_Rotation, 0, 0);
+
+        float angleDir = PlayerTransform.transform.eulerAngles.y * (Mathf.PI / 180.0f);
+        Vector3 dir1 = new Vector3(Mathf.Sin(angleDir), 0, Mathf.Cos(angleDir));
+        //Vector3 dir2 = new Vector3(-Mathf.Cos(angleDir), 0, Mathf.Sin(angleDir));
+
+
+    }
+}
+
---
+++ Onryo/Assets/script/PlayerCon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCon : MonoBehaviour {
+
+    public float speed = 3f;//移動速度
+    public GameObject Player;
+    public float roatspeed = 2;//回転速度
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float angle = Input.GetAxis("Horizontal") * roatspeed;
+        Vector3 playerpos = Player.transform.position;
+        transform.RotateAround(playerpos, Vector3.up, angle);
+        if (Input.GetKey(KeyCode.W) || Input.GetKey("up"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = 2;
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                speed = 4;
+            }
+        }
+    }
+}
