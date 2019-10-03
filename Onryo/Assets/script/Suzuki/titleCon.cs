4d751bd9feea651a35fbdab6698cdee30ab3a341
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2019-10-03 12:32:25 +0900

    titleScene改良 Scene移動追加

---
+++ Onryo/Assets/script/Suzuki/titleCon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleCon : MonoBehaviour
+{
+    AudioSource audio;
+    public AudioClip Start_SE;
+
+	// Use this for initialization
+	void Start ()
+    {
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            FadeCon.isFade1 = true;
+            FadeCon.isFadeOut1 = true;
+            audio.PlayOneShot(Start_SE);
+            Invoke("Scene", 3.0f);
+        }
+	}
+
+    void Scene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
