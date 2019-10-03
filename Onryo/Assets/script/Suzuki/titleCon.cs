140a896189a2e3f5691e6edf47a9458002047795
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2019-10-03 23:51:05 +0900

    titleScene完成

--- Onryo/Assets/script/Suzuki/titleCon.cs
+++ Onryo/Assets/script/Suzuki/titleCon.cs
@@ -5,13 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class titleCon : MonoBehaviour
 {
-    AudioSource audio;
     public AudioClip Start_SE;
+    public AudioClip title_BGM;
+    AudioSource audio;
 
 	// Use this for initialization
 	void Start ()
     {
-        audio = GetComponent<AudioSource>();
+        audio = gameObject.GetComponent<AudioSource>();
+        audio.clip = title_BGM;
+        audio.Play();
 	}
 	
 	// Update is called once per frame
@@ -21,9 +24,9 @@ public class titleCon : MonoBehaviour
         {
             FadeCon.isFade1 = true;
             FadeCon.isFadeOut1 = true;
-            audio.PlayOneShot(Start_SE);
-            audio.Stop();
-            Invoke("Scene", 3.0f);
+            audio.clip = Start_SE;
+            audio.Play();
+            Invoke("Scene", 4.35f);
         }
 	}
 
